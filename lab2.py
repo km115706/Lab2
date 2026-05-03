67165ddb0d9c006b83ba2e5f9b8f6e8d091181c6
KAUNGMYATMIN <km115706@gmail.com>
2026-05-03 17:13:19 +0800

    final version

--- lab2.py
+++ lab2.py
@@ -1,15 +1,12 @@
 #Exercise 2 -Lab2
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
-
 def display_main_menu():
-    print("“Enter some numbers separated by commas (e.g. 5, 67,32)")
-
+    print("Enter some numbers separated by commas (e.g. 5, 67,32)")
 def get_user_input():
     print("get User Input")
     userinput=input()
-    strlist=userinput.split()
+    strlist=userinput.split(",")
     float_list=[]
-    for i in strlist():
+    for i in strlist:
         float_list.append(float(i))
     return float_list
 
@@ -21,14 +18,26 @@ def calc_average(List):
 
 def find_min_max(List):
     print("find_min_max")
+    temp_list=List.copy()
+    temp_list.sort()
+    print(temp_list[0] , temp_list[-1])
 
 def sort_temperature (List):
     print("sort_temperature")
+    sorted_list=sorted(List)
+    print("sorted= " , sorted_list)
+    return sorted_list
 
 def calc_median_temperature (List):
     print("calc_median_temperature ")
-
-
+    sorted_list=sorted(List)
+    number=len(sorted_list)
+    if number % 2 == 0:
+        median = (sorted_list[number//2 - 1] + sorted_list[number//2]) / 2
+    else:
+        median = sorted_list[number//2]
+    print("Median Temperature is:", median)
+    return median
 
 
 def main():
@@ -36,8 +45,11 @@ def main():
     display_main_menu()
     List = get_user_input()
     print(List)
-
-
+    
+    calc_average(List)
+    find_min_max(List)
+    sort_temperature(List)
+    calc_median_temperature(List)
 
 if __name__ == "__main__":
     main()
